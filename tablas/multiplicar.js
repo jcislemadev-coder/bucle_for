db90174ef2579e0fd0b135cb3cbc2a90566eaba8
jcislemadev-coder <jcislemadev@gmail.com>
2026-05-03 15:48:19 -0500

    inclusion de js multiplicacion por 3

---
+++ tablas/multiplicar.js
@@ -0,0 +1,10 @@
+function generarTablas(){
+    let contenedor = document.getElementById("idcontenedor");
+    let contenido = "";
+    let numero = 3;
+
+    for(let i=0; i<11 ; i++){     
+        contenido = contenido + "<div class = 'item'>" + numero + " x " + i + " = " + (numero*i) + "</div>";
+        }           
+        contenedor.innerHTML = contenido;
+    }

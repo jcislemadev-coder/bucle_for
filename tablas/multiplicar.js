2ee5e3cfe8039b27089a5b64886f823f901725de
jcislemadev-coder <jcislemadev@gmail.com>
2026-05-03 16:21:57 -0500

    cambios en el diseño final con IA

--- tablas/multiplicar.js
+++ tablas/multiplicar.js
@@ -1,10 +1,11 @@
 function generarTablas(){
     let contenedor = document.getElementById("idcontenedor");
     let contenido = "";
-    let numero = 5;
+    let numero = document.getElementById("numero").value;
+    let numeroE = parseInt(numero);
 
     for(let i=0; i<11 ; i++){     
-        contenido = contenido + "<div class = 'item'>" + numero + " x " + i + " = " + (numero*i) + "</div>";
+        contenido = contenido + "<div class = 'item'>" + numeroE + " x " + i + " = " + (numeroE*i) + "</div>";
         }           
         contenedor.innerHTML = contenido;
     }
